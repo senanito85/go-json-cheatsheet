dd559ebefb7e2d8c6f22e92bbe57608617325b13
Sanan Ibrahimov <sibrahimov@woolworths.com.au>
2024-04-05 17:05:33 +1100

    Updated the file structure

--- text_to_struct.go
+++ text_to_struct.go
@@ -1,30 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
 type Dog struct {
-    Breed         string
-    Name          string
-    FavoriteTreat string
-    Age           int
+	Breed         string
+	Name          string
+	FavoriteTreat string
+	Age           int
 }
 
 func main() {
-    input := `{
+	input := `{
         "Breed": "Golden Retriever",
         "Age": 8,
         "Name": "Paws",
         "FavoriteTreat": "Kibble"
     }`
 
-    var dog Dog
+	var dog Dog
 
-    err := json.Unmarshal([]byte(input), &dog)
-    if err != nil {
-        log.Fatalf("Unable to marshal JSON due to %s", err)
-    }
+	err := json.Unmarshal([]byte(input), &dog)
+	if err != nil {
+		log.Fatalf("Unable to marshal JSON due to %s", err)
+	}
 
-    fmt.Printf(
-        "%s is a %d years old %s who likes %s\n",
-        dog.Name,
-        dog.Age,
-        dog.Breed,
-        dog.FavoriteTreat,
-    )
+	fmt.Printf(
+		"%s is a %d years old %s who likes %s\n",
+		dog.Name,
+		dog.Age,
+		dog.Breed,
+		dog.FavoriteTreat,
+	)
 }
