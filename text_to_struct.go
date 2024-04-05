0744205f097fc42ceee279e857d32050ab0bdbfd
Sanan Ibrahimoff <36265361+senanito85@users.noreply.github.com>
2024-04-05 16:58:08 +1100

    Create text_to_struct.go

---
+++ text_to_struct.go
@@ -0,0 +1,30 @@
+type Dog struct {
+    Breed         string
+    Name          string
+    FavoriteTreat string
+    Age           int
+}
+
+func main() {
+    input := `{
+        "Breed": "Golden Retriever",
+        "Age": 8,
+        "Name": "Paws",
+        "FavoriteTreat": "Kibble"
+    }`
+
+    var dog Dog
+
+    err := json.Unmarshal([]byte(input), &dog)
+    if err != nil {
+        log.Fatalf("Unable to marshal JSON due to %s", err)
+    }
+
+    fmt.Printf(
+        "%s is a %d years old %s who likes %s\n",
+        dog.Name,
+        dog.Age,
+        dog.Breed,
+        dog.FavoriteTreat,
+    )
+}
